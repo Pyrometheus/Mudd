2c500816518c8d2f7969acfbe9889bc408f0e1f9
KodyJKing <kody.j.king@gmail.com>
2016-11-13 19:44:56 -0800

    3D!

--- Mudd/Assets/Scripts/DelayFollowCamera.cs
+++ Mudd/Assets/Scripts/DelayFollowCamera.cs
@@ -5,16 +5,19 @@ public class DelayFollowCamera : MonoBehaviour {
 
     public float rate;
     public Transform target;
+    Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-	
+        offset = transform.position - target.position;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 tpos = target.position;
-        Vector3 pos = transform.position;
-        transform.Translate(new Vector3(tpos.x - pos.x, tpos.y - pos.y, 0) * rate);
+        Vector3 motion = (target.position - transform.position) + offset;
+        transform.position += new Vector3(motion.x, 0, motion.z) * rate;
+        //Vector3 tpos = target.position;
+        //Vector3 pos = transform.position;
+        //transform.position += new Vector3(tpos.x - pos.x, 0, tpos.z - pos.z) * rate;
     }
 }
--- Mudd/Assets/Scripts/PlayerController.cs
+++ Mudd/Assets/Scripts/PlayerController.cs
@@ -5,22 +5,22 @@ public class PlayerController : MonoBehaviour {
 
     public float walkForce = 50;
 
-    Rigidbody2D body;
+    Rigidbody body;
 
 	// Use this for initialization
 	void Start () {
-        body = GetComponent<Rigidbody2D>();
+        body = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.W))
-            body.AddForce(Vector2.up * walkForce);
+            body.AddForce(Vector3.forward * walkForce);
         if (Input.GetKey(KeyCode.S))
-            body.AddForce(Vector2.down * walkForce);
+            body.AddForce(Vector3.back * walkForce);
         if (Input.GetKey(KeyCode.A))
-            body.AddForce(Vector2.left * walkForce);
+            body.AddForce(Vector3.left * walkForce);
         if (Input.GetKey(KeyCode.D))
-            body.AddForce(Vector2.right * walkForce);
+            body.AddForce(Vector3.right * walkForce);
     }
 }
